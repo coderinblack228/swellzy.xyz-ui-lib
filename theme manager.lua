13429e366eb4c628fa538e98daa6d2a9abad68b5
coderinblack228 <99586564+coderinblack228@users.noreply.github.com>
2022-07-01 15:37:39 +0400

    Update theme manager.lua

--- theme manager.lua
+++ theme manager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = '$wellzy.xyz Configs/Counter Blox/'
+	ThemeManager.Folder = '$wellzy.xyz/themes/'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
