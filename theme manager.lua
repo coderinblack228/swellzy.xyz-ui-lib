ea25d5b047403b45d4bf1c2cacb54cf412a09022
coderinblack228 <99586564+coderinblack228@users.noreply.github.com>
2022-07-01 15:37:12 +0400

    Update theme manager.lua

--- theme manager.lua
+++ theme manager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'LinoriaLibSettings'
+	ThemeManager.Folder = '$wellzy.xyz Configs/Counter Blox/'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
